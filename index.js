24c9badb014ec38ac4597e8be3b60a19342ce79b
RkSantoz <rayssa19765@gmail.com>
2024-01-11 22:20:00 -0300

    projeto erro

---
+++ index.js
@@ -0,0 +1,15 @@
+let personagensElementares = [
+    ["OndaSuave", 100],
+    ["TerraTremor", 1500],
+    ["VentoVoraz", 3000],
+    ["GeloGélido", 12000],
+    ["RelâmpagoRápido", 8500],
+    ["PlantaProtetora", 4320],
+    ["MetalMagnético", 9750],
+    ["LuzLuminosa", 6180],
+    ["SombraSilenciosa", 7700],
+    ["FogoFaísca", 10100],
+    ]
+    
+    console.log("Qual seu nome aventureiro ? ")
+    console.log(personagensElementares[0][0])
